b7110b7d548337480a5b3cb2616e0ffde65671f0
taiki-nd <taiki.nd.real@gmail.com>
2021-05-18 00:30:26 +0900

    scroll_animation_now

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-window.addEventListener('load', function(){
-
-    const status = document.getElementsByClassName("navi")
-    const current = document.getElementsByClassName("content_status")
-
-    status.forEach(function(list){
-        list.addEventListener('click', function(){
-            value = list.innerHTML
-            current.innerHTML = value
-        })
-    })
-
-
-})
-
-
 function ScrollWindow(elem) {
     var element = document.getElementById(elem);
     var rect = element.getBoundingClientRect();
     var elemtop = rect.top + window.pageYOffset;
     document.documentElement.scrollTop = elemtop;
   }
+
+const hedderAnimation = document.getElementById("header")
+hedderAnimation.addEventListener('scroll', function(){
+    console.log("ok")
+})
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('header').css('opacity', 0.8);
+        } else {
+            $('header').css('opacity', 1);
+        }
+    });
+});
