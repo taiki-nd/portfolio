e2621104ee89c1fd233e40b93fc4ac229729ba7d
taiki-nd <taiki.nd.real@gmail.com>
2021-06-03 23:38:12 +0900

    advance

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+$(function(){
+
+  $('.toggle_btn').on('click', function(){
+    if (!$('#header').hasClass('open')){
+      $('#header').addClass('open');
+    }else{
+      $('#header').removeClass('open');
+    }
+  });
+
+  $('#mask').on('click', function(){ 
+    $('#header').removeClass('open');
+  });
 
 
-function ScrollWindow(elem) {
-    var element = document.getElementById(elem);
-    var rect = element.getBoundingClientRect();
-    var elemtop = rect.top + window.pageYOffset;
-    document.documentElement.scrollTop = elemtop;
-  }
+  $('a[href^="#"]').click(function(){
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+  
 
+  $(window).scroll(function(){
+    $('.fadein').each(function(){
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+      if(scroll > target - windowHeight + 200){
+        $(this).css('opacity','1');
+        $(this).css('transform', 'translateY(0)');
+      }
+    });
+  });
 
-
+});
