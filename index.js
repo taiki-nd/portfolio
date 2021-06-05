f02bad5d76aa95dade991b66b367e62c4fb15fc2
taiki-nd <taiki.nd.real@gmail.com>
2021-06-06 01:38:52 +0900

    responsive

--- index.js
+++ index.js
@@ -21,17 +21,30 @@ $(function(){
     return false;
   });
   
-
-  $(window).scroll(function(){
-    $('.fadein').each(function(){
-      let scroll = $(window).scrollTop();
-      let target = $(this).offset().top;
-      let windowHeight = $(window).height();
-      if(scroll > target - windowHeight + 200){
-        $(this).css('opacity','1');
-        $(this).css('transform', 'translateY(0)');
-      }
+  if (window.matchMedia( '(min-width: 600px)' ).matches){
+    $(window).scroll(function(){
+      $('.fadein').each(function(){
+        let scroll = $(window).scrollTop();
+        let target = $(this).offset().top;
+        let windowHeight = $(window).height();
+        if(scroll > target - windowHeight + 200){
+          $(this).css('opacity','1');
+          $(this).css('transform', 'translateY(0)');
+        }
+      });
     });
-  });
+  }else{
+    $(window).scroll(function(){
+      $('.fadein').each(function(){
+        let scroll = $(window).scrollTop();
+        let target = $(this).offset().top;
+        let windowHeight = $(window).height();
+        if(scroll > target - windowHeight + 100){
+          $(this).css('opacity','1');
+          $(this).css('transform', 'translateY(0)');
+        }
+      });
+    });
+  }
 
 });
